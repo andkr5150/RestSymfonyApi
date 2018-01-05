ef90f0a12e22c84079ba3047d5e63eef4c447b10
andrey <andkr5150@gmail.com>
2018-01-05 16:12:17 +0200

    add action UpDel

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -57,5 +57,56 @@ class UserController extends FOSRestController
         return new View("User Added Successfully", Response::HTTP_OK);
     }
 
+    /**
+    * @Rest\Put("/user/{id}")
+    */
+    public function updateAction($id,Request $request)
+    {
+        $data = new User;
+        $name = $request->get('name');
+        $role = $request->get('role');
+        $sn = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+        if (empty($user)) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+        elseif(!empty($name) && !empty($role)){
+            $user->setName($name);
+            $user->setRole($role);
+            $sn->flush();
+            return new View("User Updated Successfully", Response::HTTP_OK);
+        }
+        elseif(empty($name) && !empty($role)){
+            $user->setRole($role);
+            $sn->flush();
+            return new View("role Updated Successfully", Response::HTTP_OK);
+        }
+        elseif(!empty($name) && empty($role)){
+            $user->setName($name);
+            $sn->flush();
+            return new View("User Name Updated Successfully", Response::HTTP_OK);
+        }
+        else return new View("User name or role cannot be empty", Response::HTTP_NOT_ACCEPTABLE);
+    }
+
+    /**
+     * @Rest\Delete("/user/{id}")
+     */
+    public function deleteAction($id)
+    {
+        $data = new User;
+        $sn = $this->getDoctrine()->getManager();
+        $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
+        if (empty($user)) {
+            return new View("user not found", Response::HTTP_NOT_FOUND);
+        }
+        else {
+            $sn->remove($user);
+            $sn->flush();
+        }
+        return new View("deleted successfully", Response::HTTP_OK);
+    }
+
+
 
 }
